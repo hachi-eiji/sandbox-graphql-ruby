c19534462a34ff16d02e8f26d3842185548085a1
Eiji Hachiya <hachiyae@gmail.com>
2023-09-12 23:58:48 +0900

    chore: テストデータ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+today = Time.zone.today
+days = ((today - 1.weeks)..today).to_a
+
+10.times do |i|
+  user = User.new(name: "user#{i}")
+
+  10.times do |j|
+    user.books << Book.new(name: "book#{j}", purchase_on: days.sample)
+  end
+  user.save!
+end
