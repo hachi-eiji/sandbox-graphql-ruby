4bdb28af5d3357db218999ff0df0c9d2779cd24e
Eiji Hachiya <hachiyae@gmail.com>
2023-09-12 23:45:22 +0900

    feat: booksテーブルの定義

--- db/migrate/20230912144216_create_books.rb
+++ db/migrate/20230912144216_create_books.rb
@@ -1,7 +1,9 @@
 class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
-
+      t.references :user, null: false
+      t.string :name, null: false
+      t.date :purchase_on, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_12_144013) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_12_144216) do
+  create_table "books", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "name", null: false
+    t.date "purchase_on", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
