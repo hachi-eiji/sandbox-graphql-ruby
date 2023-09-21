f46e978d22898d96fdae7155da5faf4ae8144fd5
Eiji Hachiya <hachiyae@gmail.com>
2023-09-22 00:45:37 +0900

    feat: has_book

--- app/graphql/types/preload_model/user_type.rb
+++ app/graphql/types/preload_model/user_type.rb
@@ -6,6 +6,7 @@ module Types
       field :id, ID, null: false
       field :name, String, null: false
       field :purchase_books, [Types::PreloadModel::BookType]
+      field :has_book, Boolean, method: :has_book?
     end
   end
 end
