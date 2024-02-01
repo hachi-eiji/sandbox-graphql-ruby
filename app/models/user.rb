2df5eaf0513cb2cd7542215b48580da122b0cc9f
Eiji Hachiya <hachiyae@gmail.com>
2024-02-01 21:32:19 +0900

    add ignore column

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  self.ignored_columns += [:named]
   # has_manyでスコープを指定するのはよくないです
   has_many :purchase_books, -> { order(:purchase_on) }, class_name: 'Book'
   has_many :old_purchase_books, -> { old_books }, class_name: 'Book'
